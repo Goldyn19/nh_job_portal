eb5cb5a99f74a48be64bf08c2e893b80cb849c12
Goldyn19 <nwaezegolden2002@gmail.com>
2025-01-10 12:44:08 +0100

    added pymysql to requirements.txt

--- jobPortal/settings.py
+++ jobPortal/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from datetime import timedelta
 from pathlib import Path
 from decouple import config
+import pymysql
+
+# Use PyMySQL as MySQLdb
+pymysql.install_as_MySQLdb()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
