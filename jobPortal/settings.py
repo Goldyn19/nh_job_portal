4c0d82f727a7a88786ceceb0df572622a5c51384
Goldyn19 <nwaezegolden2002@gmail.com>
2025-01-11 09:38:43 +0100

    added render to allowed host

--- jobPortal/settings.py
+++ jobPortal/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '.onrender.com', '0.0.0.0', 'localhost']
 
 
 # Application definition
